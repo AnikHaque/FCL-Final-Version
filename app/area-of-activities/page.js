5bb6b07ff905e8f1a2ce42e06e0808db93ed61c2
AnikHaque <ekramul15-811@diu.edu.bd>
2024-09-05 16:03:15 +0600

    added

--- app/area-of-activities/page.js
+++ app/area-of-activities/page.js
@@ -174,8 +174,8 @@ const Activities = () => {
       </div>
 
       {selectedActivity && (
-        <dialog open className="modal">
-          <div className="modal-box">
+        <dialog open className="modal ">
+          <div className="modal-box bg-green-200">
             <form method="dialog">
               <button
                 className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 "
